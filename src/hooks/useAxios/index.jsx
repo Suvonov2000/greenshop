0c25beb874bfac69388363f1b717c04baba9c372
Suvonov2000 <suvonov200@gmail.com>
2025-05-15 05:06:32 +0900

    some corrections

--- src/hooks/useAxios/index.jsx
+++ src/hooks/useAxios/index.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const useAxios = () => {
   return async ({ url, method = "GET", params, data, headers }) => {
     return await axios({
-      url: `https://greenshopapi.rakhmatov1020.uz//api${url}`,
+      url: `http://localhost:8080/api${url}`,
       method,
       params: {
         access_token: "64bebc1e2c6d3f056a8c85b7",
